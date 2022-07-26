f7592447c2b1455a74a535045dcbdc411ddf19db
chefmomota <momota@pancakeswap.com>
2022-07-26 15:31:26 +0800

    fix: Logo url

--- src/top-100.ts
+++ src/top-100.ts
@@ -111,7 +111,7 @@ const getTokens = async (): Promise<BitqueryEntity[]> => {
 const getTokenLogo = (address: string): string => {
   // Note: fs.existsSync can't be used here because its not case sensetive
   if (logoFiles.includes(`${address}.png`)) {
-    return `https://tokens.pancakeswap.finance/images/56/56/${address}.png`;
+    return `https://tokens.pancakeswap.finance/images/56/${address}.png`;
   }
 
   return `${TWLogo[56]}/${address}/logo.png`;
