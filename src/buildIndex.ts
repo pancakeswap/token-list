c4fd2ccc3d816165bfa3feb26cf840feb2e4438b
AtlasRewards <marfanstore24@gmail.com>
2024-01-16 18:14:48 +0100

    Hinzufügen des Logos für mein Token

--- src/buildIndex.ts
+++
@@ -1,7 +0,0 @@
-
-export const buildIndex = async (lists: Record<string, unknown>) => {
-    const html = await Bun.file(`src/index.html`).text()
-
-    const newContent = html.replace('`<!--LISTS-->`', JSON.stringify(lists));
-    await Bun.write('lists/index.html', newContent)
-}
